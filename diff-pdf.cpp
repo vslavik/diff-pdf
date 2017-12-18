b57e8cf97a8123999b3ca61cce4c097143b8927b
Michael Frey <michael.frey@gmx.ch>
2017-12-18 21:21:47 +0100

    clean up

--- diff-pdf.cpp
+++ diff-pdf.cpp
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+ 
 #include "bmpviewer.h"
 #include "gutter.h"
 
@@ -390,12 +391,6 @@ bool doc_compare(PopplerDocument *doc1, PopplerDocument *doc2,
                               pages_total
                           )
                        );
-        }
-
-        poppler_page_get_size(poppler_document_get_page(doc1, page), &w, &h);
-        if ( pdf_output )
-        {
-           cairo_pdf_surface_set_size (surface_out, w, h);
         }
 
         PopplerPage *page1 = page < pages1
